46979cbd27eee4ec30ebe27bf29f6f3b864c8bb1
rsevig00 <47629851+rsevig00@users.noreply.github.com>
2021-01-06 18:16:13 +0100

    Add files via upload

---
+++ MaxFilmRatingReducer.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class MaxFilmRatingReducer extends Mapper<LongWritable, Text, Text, DoubleWritable> extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+	@Override
+	public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
+		int maxValue = Integer.MIN_VALUE;
+		for (DoubleWritable value : values) {
+			maxValue = Math.max(maxValue, value.get());
+	    }
+	    context.write(key, new DoubleWritable(maxValue));
+	}
+}
